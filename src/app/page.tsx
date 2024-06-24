ec8d0c9c2469897c966dc1f446b62206a8cc5a73
Rafael Fonseca <rafael.silvanofonseca@gmail.com>
2024-06-24 12:58:01 -0400

    Greeting paragraph

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,16 +10,12 @@ export default function Home() {
   return (
     <Container>
       <span className="text-4xl">👋</span>
-      <Heading className="font-black">Hello there! I&apos;m John</Heading>
+      <Heading className="font-black">Hello there! I&apos;m Rafael</Heading>
       <Paragraph className="max-w-xl mt-4">
-        I&apos;m a full-stack developer that loves{" "}
-        <Highlight>building products</Highlight> and web apps that can impact
-        millions of lives
-      </Paragraph>
-      <Paragraph className="max-w-xl mt-4">
-        I&apos;m a senior software engineer with{" "}
-        <Highlight>7 years of experience</Highlight> building scalable web apps
-        that are performance optimized and good looking.
+        I&apos;m a software developer based in {" "}
+        <Highlight>Toronto</Highlight>. My passion for 
+        learning has led me to explore web development, 
+        machine learning, and Web3.
       </Paragraph>
       <Heading
         as="h2"
