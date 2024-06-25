6be0eca666c595f45bfe6bafcc970cd2063eb04e
Rafael Fonseca <rafael.silvanofonseca@gmail.com>
2024-06-25 08:02:04 -0400

    Sidebar and layout changes

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -109,16 +109,17 @@ const SidebarHeader = () => {
   return (
     <div className="flex space-x-2">
       <Image
-        src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1760&q=80"
+        src="https://res.cloudinary.com/dkhbzvmeq/image/upload/v1719316512/IMG_2421_jtd9hn.jpg"
         alt="Avatar"
         height="40"
         width="40"
         className="object-cover object-top rounded-full flex-shrink-0"
       />
       <div className="flex text-sm flex-col">
-        <p className="font-bold text-primary">John Doe</p>
-        <p className="font-light text-secondary">Developer</p>
+        <p className="font-bold text-primary">Rafael Fonseca</p>
+        <p className="font-light text-secondary">Software Developer</p>
       </div>
     </div>
   );
 };
+
