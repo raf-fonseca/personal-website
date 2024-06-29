5e6484fdca450d623cc1fa1cd6aadf11a2bc3032
Rafael Fonseca <rafael.silvanofonseca@gmail.com>
2024-06-29 12:37:18 -0400

    resume section

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -11,11 +11,6 @@ export default function Home() {
     <Container>
       <span className="text-4xl">💼</span>
       <Heading className="font-black">Work History</Heading>
-      <Paragraph className="max-w-xl mt-4">
-        I&apos;m a full-stack developer that loves{" "}
-        <Highlight>building products</Highlight> and web apps that can impact
-        millions of lives
-      </Paragraph>
       <WorkHistory />
     </Container>
   );
