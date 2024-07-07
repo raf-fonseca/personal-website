ba837474f05afa2ad091fc7d30b5e6d38f69cad2
Rafael Fonseca <rafael.silvanofonseca@gmail.com>
2024-07-07 13:06:18 -0400

    Removing project

--- src/constants/products.tsx
+++ src/constants/products.tsx
@@ -1,37 +1,37 @@
 export const products = [
-  {
-    href: "https://gomoonbeam.com",
-    title: "Moonbeam",
-    description:
-      "Never write from scratch again with Moonbeam, your AI first writing tool",
-    thumbnail: "/images/sidefolio-moonbeam.png",
-    images: [
-      "/images/sidefolio-moonbeam.png",
-      "/images/sidefolio-moonbeam-2.png",
-    ],
-    stack: ["Nextjs", "Tailwindcss"],
-    slug: "moonbeam",
-    content: (
-      <div>
-        <p>
-          Sit eiusmod ex mollit sit quis ad deserunt. Sint aliqua aliqua ullamco
-          dolore nulla amet tempor sunt est ipsum. Dolor laborum eiusmod
-          cupidatat consectetur velit ipsum. Deserunt nisi in culpa laboris
-          cupidatat elit velit aute mollit nisi. Officia ad exercitation laboris
-          non cupidatat duis esse velit ut culpa et.{" "}
-        </p>
-        <p>
-          Exercitation pariatur enim occaecat adipisicing nostrud adipisicing
-          Lorem tempor ullamco exercitation quis et dolor sint. Adipisicing sunt
-          sit aute fugiat incididunt nostrud consequat proident fugiat id.
-          Officia aliquip laborum labore eu culpa dolor reprehenderit eu ex enim
-          reprehenderit. Cillum Lorem veniam eu magna exercitation.
-          Reprehenderit adipisicing minim et officia enim et veniam Lorem
-          excepteur velit adipisicing et Lorem magna.
-        </p>{" "}
-      </div>
-    ),
-  },
+  // {
+  //   href: "https://gomoonbeam.com",
+  //   title: "Moonbeam",
+  //   description:
+  //     "Never write from scratch again with Moonbeam, your AI first writing tool",
+  //   thumbnail: "/images/sidefolio-moonbeam.png",
+  //   images: [
+  //     "/images/sidefolio-moonbeam.png",
+  //     "/images/sidefolio-moonbeam-2.png",
+  //   ],
+  //   stack: ["Nextjs", "Tailwindcss"],
+  //   slug: "moonbeam",
+  //   content: (
+  //     <div>
+  //       <p>
+  //         Sit eiusmod ex mollit sit quis ad deserunt. Sint aliqua aliqua ullamco
+  //         dolore nulla amet tempor sunt est ipsum. Dolor laborum eiusmod
+  //         cupidatat consectetur velit ipsum. Deserunt nisi in culpa laboris
+  //         cupidatat elit velit aute mollit nisi. Officia ad exercitation laboris
+  //         non cupidatat duis esse velit ut culpa et.{" "}
+  //       </p>
+  //       <p>
+  //         Exercitation pariatur enim occaecat adipisicing nostrud adipisicing
+  //         Lorem tempor ullamco exercitation quis et dolor sint. Adipisicing sunt
+  //         sit aute fugiat incididunt nostrud consequat proident fugiat id.
+  //         Officia aliquip laborum labore eu culpa dolor reprehenderit eu ex enim
+  //         reprehenderit. Cillum Lorem veniam eu magna exercitation.
+  //         Reprehenderit adipisicing minim et officia enim et veniam Lorem
+  //         excepteur velit adipisicing et Lorem magna.
+  //       </p>{" "}
+  //     </div>
+  //   ),
+  // },
   {
     href: "https://github.com/raf-fonseca/Network-Converter",
     title: "Network Converter",
