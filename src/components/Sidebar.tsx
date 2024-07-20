631ad3a85fdd072ed721ce1a1476e84741efcea6
Rafael Fonseca <rafael.silvanofonseca@gmail.com>
2024-07-19 21:36:20 -0400

    changing size of menu button

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -41,10 +41,10 @@ export const Sidebar = () => {
         )}
       </AnimatePresence>
       <button
-        className="fixed lg:hidden bottom-4 right-4 h-8 w-8 border border-neutral-200 rounded-full backdrop-blur-sm flex items-center justify-center z-10"
+        className="fixed lg:hidden bottom-4 right-4 h-15 w-15 border border-neutral-200 rounded-full backdrop-blur-sm flex items-center justify-center z-10"
         onClick={() => setOpen(!open)}
       >
-        <IconLayoutSidebarRightCollapse className="h-4 w-4 text-secondary" />
+        <IconLayoutSidebarRightCollapse className="h-15 w-15 text-secondary" />
       </button>
     </>
   );
